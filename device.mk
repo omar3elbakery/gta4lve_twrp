85e1d776c5285475c84e02a59f4e86b59df3b224
CatDev3 <168304418+omar3elbakery@users.noreply.github.com>
2025-11-29 21:38:08 +0200

    Add fastboot implementation mock to device.mk

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_PACKAGES += \
 android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_SHIPPING_API_LEVEL := 30
